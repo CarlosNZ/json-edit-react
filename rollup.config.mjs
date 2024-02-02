321049449b07540447a2eb82a816358ef92d8127
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2024-02-02 13:29:07 +1300

    Add bundle size to rollup output

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,9 +1,10 @@
 import typescript from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
-import sizes from 'rollup-plugin-sizes'
+// import sizes from 'rollup-plugin-sizes'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import styles from 'rollup-plugin-styles'
 import terser from '@rollup/plugin-terser'
+import bundleSize from 'rollup-plugin-bundle-size'
 
 export default [
   {
@@ -22,8 +23,9 @@ export default [
       styles(),
       peerDepsExternal({ includeDependencies: true }),
       typescript({ module: 'ESNext', target: 'es6' }),
-      sizes(),
+      // sizes(),
       terser({ mangle: false }),
+      bundleSize(),
     ],
     external: [],
   },
