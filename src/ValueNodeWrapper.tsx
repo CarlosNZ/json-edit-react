4402f7171f0b198f6baeb14e3cd0aa01a638c053
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2024-07-23 14:54:36 +1200

    Squashed commit of the following:
    commit 07f051f5f89a08209e303039a379bfbb86eb337b
    Author: Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
    Date:   Tue Jul 23 14:39:30 2024 +1200
    
        Improve keyboard handler for Boolean and Null

--- src/ValueNodeWrapper.tsx
+++ src/ValueNodeWrapper.tsx
@@ -358,10 +358,6 @@ const getInputComponent = (dataType: DataType, inputProps: InputProps) => {
       return <BooleanValue {...inputProps} value={value as boolean} />
     case 'null':
       return <NullValue {...inputProps} />
-    case 'object':
-      return <ObjectValue {...inputProps} value={value} />
-    case 'array':
-      return <ArrayValue {...inputProps} />
     default:
       return <InvalidValue {...inputProps} />
   }
