0043406ec49735d55e1b0e51447eeeaf56564156
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2025-06-07 12:09:08 +1200

    Tidy demo data

--- custom-component-library/src/App.tsx
+++ custom-component-library/src/App.tsx
@@ -59,7 +59,7 @@ function App() {
           },
           {
             ...MarkdownNodeDefinition,
-            condition: ({ key }) => key === 'intro',
+            condition: ({ key }) => key === 'Intro',
             hideKey: true,
           },
         ]}
