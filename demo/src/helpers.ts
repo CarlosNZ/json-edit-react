10eec710dbbe2d6ee43411335e9841449b7500c4
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2025-06-14 21:36:23 +1200

    Custom JSON.stringify in Demo app to handle non-JSON types

--- demo/src/helpers.ts
+++ demo/src/helpers.ts
@@ -3,4 +3,20 @@ import { JsonData } from '@json-edit-react'
 export const truncate = (string: string, length = 200) =>
   string.length < length ? string : `${string.slice(0, length - 2).trim()}...`
 
-export const getLineHeight = (data: JsonData) => JSON.stringify(data, null, 2).split('\n').length
+export const getLineHeight = (data: JsonData) => jsonStringify(data).split('\n').length
+
+// Special JSON.stringify with custom replacer functions for special types
+const jsonStringify = (data: JsonData) =>
+  JSON.stringify(
+    data,
+    (_, value) => {
+      if (typeof value === 'bigint') {
+        return value.toString()
+      }
+      if (typeof value === 'symbol') {
+        return value.toString()
+      }
+      return value
+    },
+    2
+  )
